d4d680b3d517a2a7e59fb6637b73d3f4979a615e
Pratik Kumar <pratikkumar@Pratiks-MacBook-Air.local>
2024-05-14 00:50:43 +0530

    creating nested tags

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const heading = React.createElement('h1', { class: 'heading', id: 'heading' }, 'Hello World using React');
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'I am a h1 tag'),
+    React.createElement('h2', {}, 'I am a h2 tag'),
+  ])
+);
+// const heading = React.createElement('h1', { class: 'heading', id: 'heading' }, 'Hello World using React');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
